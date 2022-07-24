96a2cdd13218efa4e8a47fe9eea0beaf1d373996
Hugo Majerczyk <Superseth794@gmail.com>
2022-07-24 13:05:33 +0200

    Antoine

--- contracts/vaultCRV/IgaugeController.sol
+++ contracts/vaultCRV/IgaugeController.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 interface IgaugeController {
-    function vote_for_gauge_weights(address _gaugeAddress, _user_weight: uint256) external;
-    mapping(address => uint256) vote_user_power;
+    function vote_for_gauge_weights(address _gaugeAddress, uint256 _user_weight) external;
     function vote_user_power(address) external returns(uint256);
 }
