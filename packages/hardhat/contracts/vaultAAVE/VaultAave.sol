6e688a416e207575ebbc8d3a6b18b6c154c97874
Hugo Majerczyk <Superseth794@gmail.com>
2022-07-24 00:25:28 +0000

    Move pal contracts to correct folder

--- packages/hardhat/contracts/vaultAAVE/VaultAave.sol
+++ packages/hardhat/contracts/vaultAAVE/VaultAave.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./IPalPool.sol";
-import "./IStakedAave.sol";
-import "./IPaladinController.sol";
+import "../paladin/IPalPool.sol";
+import "../paladin/IStakedAave.sol";
+import "../paladin/IPaladinController.sol";
 
 //TODO safeERC20
 contract VaultAave is Ownable {
