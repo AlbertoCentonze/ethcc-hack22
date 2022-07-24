8cf8fb3217357b3eb4101cabb067430699b5d65f
Alberto Centonze <albicento.ac@gmail.com>
2022-07-24 03:19:52 +0000

    Added pure hardhat project

--- chain.sh
+++
@@ -1,1 +0,0 @@
-docker exec -ti SCAFFOLD_ETH bash -c "yarn chain"
---
+++ scripts/deploy.ts
@@ -0,0 +1,19 @@
+import { ethers } from "hardhat"
+const helpers = require("@nomicfoundation/hardhat-network-helpers")
+
+async function main() {
+  const VaultAave = await ethers.getContractFactory("VaultAave")
+  const vault = await VaultAave.deploy()
+  await vault.deployed()
+	const binanceAddress = "0xF977814e90dA44bFA03b6295A0616a897441aceC"
+	await helpers.impersonateAccount(binanceAddress)
+	const binanceSigner = await ethers.getSigner(binanceAddress)
+	await (await (await VaultAave.connect(binanceSigner)).deploy()).debug();
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
